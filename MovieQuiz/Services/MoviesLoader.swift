ce616f58819592158e9be12273af30e11ddebb7a
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-05-18 21:47:51 +0300

    fix: remarks/suggestions
    Fix, remarks/suggestions based on code review.

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -12,17 +12,7 @@ protocol MoviesLoading {
 }
 
 struct MoviesLoader: MoviesLoading {
-    // MARK: - NetworkCliend
-    private let networkClient = NetworkClient()
-    
-    // MARK: - URL
-    private var mostPopularMoviesUrl: URL {
-        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
-            preconditionFailure("Unable to construct mostPopularMoviesUrl")
-        }
-        return url
-    }
-    
+    // MARK: - MoviesLoading
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
         networkClient.fetch(url: mostPopularMoviesUrl) { result in
             switch result {
@@ -37,5 +27,16 @@ struct MoviesLoader: MoviesLoading {
                 handler(.failure(error))
             }
         }
+    }
+    
+    // MARK: - NetworkCliend
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
     }
 }
