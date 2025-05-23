eb718b7d4213a8e1aa3c7ece4657524bc2d9f8c1
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-05-23 13:16:01 +0300

    refactor: add init member
    Refactor, add init member to provide dependency injection via ctor.

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -12,6 +12,21 @@ protocol MoviesLoading {
 }
 
 struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient: NetworkRouting
+
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+
+    init(networkClient: NetworkRouting = NetworkClient()) {
+        self.networkClient = networkClient
+    }
+
     // MARK: - MoviesLoading
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
         networkClient.fetch(url: mostPopularMoviesUrl) { result in
@@ -27,16 +42,5 @@ struct MoviesLoader: MoviesLoading {
                 handler(.failure(error))
             }
         }
-    }
-    
-    // MARK: - NetworkCliend
-    private let networkClient = NetworkClient()
-    
-    // MARK: - URL
-    private var mostPopularMoviesUrl: URL {
-        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
-            preconditionFailure("Unable to construct mostPopularMoviesUrl")
-        }
-        return url
     }
 }
