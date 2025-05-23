b797b1b7cf666eaffac349dc05bf280a9a7a4554
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-05-23 13:07:00 +0300

    add: unit test members
    Add unit test members/remove deprecated mock data.

---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Roman Yaschenkov on 22.05.2025.
+//
+
+import Testing
+
+struct MoviesLoaderTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ MovieQuizTests/StubNetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  StubNetworkClient.swift
+//  MovieQuizTests
+//
+//  Created by Roman Yaschenkov on 22.05.2025.
+//
+
+import Foundation
