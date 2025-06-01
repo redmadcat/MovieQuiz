ab41f3a83e772425ccb0774528e8f3e47a262bb1
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-06-01 22:21:49 +0300

    test: add presenter test members
    Test, add presenter test/mock data members.

---
+++ MovieQuizTests/MovieQuizPresenterTests.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizPresenterTests.swift
+//  MovieQuizTests
+//
+//  Created by Roman Yaschenkov on 01.06.2025.
+//
+
+import Foundation
---
+++ MovieQuizTests/MovieQuizViewControllerMock.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerMock.swift
+//  MovieQuizTests
+//
+//  Created by Roman Yaschenkov on 01.06.2025.
+//
+
+import Foundation
